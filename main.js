73e4852a5617cb7a8d48ebdafe29ce1eb5406916
stevgonzalezb <steven.gonzalez@snagajob.com>
2022-11-21 12:21:24 -0600

    running electron

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log(`Hello from Electron 👋`)
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+      })
+      
+      win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
